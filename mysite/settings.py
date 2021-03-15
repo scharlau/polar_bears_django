b84c84fbaeb61f58d2feed1a08d9829a49518a0b
Bruce Scharlau <b.scharlau@abdn.ac.uk>
2021-03-15 10:51:17 +0000

    added heroku url

--- mysite/settings.py
+++ mysite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'ha+ub#-6vfjlw*jk-+h1nr@$0n5b!6==^xo5ss)31vkh#06$89'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ancient-reef-80308.herokuapp.com']
 
 
 # Application definition
